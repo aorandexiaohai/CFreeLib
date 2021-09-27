a8cba5b2a5091c7f8488097069220ebbfcf92cec
wuzhenhai <2663976081@qq.com>
2021-09-27 03:30:16 -0700

    code

--- Sort/csort.c
+++ Sort/csort.c
@@ -1,14 +1,28 @@
 #include "csort.h"
 
-void sort_array_merge(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf) {}
+#include <string.h>
 
-void sort_array_insertion(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf) {}
+#define ARR(idx) (void*)((char*)(arr) + (single_element_size) * (idx))
 
-void sort_array(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf,
+void sort_array_merge(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf) {}
+
+void sort_array_insertion(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf) {
+    for (int j = 1; j < element_count; j++) {
+        void* key = ARR(j);
+        int i = j - 1;
+        while (i >= 0 && cf(ARR(i), key) > 0) {
+            memcpy(ARR(i + 1), ARR(i), single_element_size);
+            i--;
+        }
+        memcpy(ARR(i + 1), key, single_element_size);
+    }
+}
+
+void sort_array(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf,
                 sort_algorithm_t sa) {
     if (sa == Merge) {
-        sort_array_merge(arr, single_element_length, element_count, cf);
+        sort_array_merge(arr, single_element_size, element_count, cf);
     } else if (sa == Insertion) {
-        sort_array_insertion(arr, single_element_length, element_count, cf);
+        sort_array_insertion(arr, single_element_size, element_count, cf);
     }
 }
--- Sort/csort.h
+++ Sort/csort.h
@@ -6,6 +6,6 @@
  */
 enum sort_algorithm_inner { Insertion = 0, Merge };
 typedef enum sort_algorithm_inner sort_algorithm_t;
-void sort_array(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf,
+void sort_array(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf,
                 sort_algorithm_t sa);
 #endif  // CSORT_H
