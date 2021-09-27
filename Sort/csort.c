82c97c3d73c6e4fcf9e36845ffb09e0eb0b884f0
wuzhenhai <2663976081@qq.com>
2021-09-27 03:18:30 -0700

    cde

--- Sort/csort.c
+++ Sort/csort.c
@@ -1,19 +1,14 @@
 #include "csort.h"
 
-void sort_array_merge(generic_data_t arr, int element_length, int element_count, compare_function_t cf) 
-{
-}
+void sort_array_merge(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf) {}
 
-void sort_array_insertion(generic_data_t arr, int element_length, int element_count, compare_function_t cf) 
-{
-}
+void sort_array_insertion(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf) {}
 
-void sort_array(generic_data_t arr, int element_length, int element_count, compare_function_t cf, sort_algorithm_t sa) 
-{
-    if(sa == sort_algorithm_t::Merge) {
-        sort_array_merge(arr, element_length, element_count, cf);
-    } else if(sa == sort_algorithm_t::Insertion) {
-        sort_array_insertion(arr, element_length, element_count, cf);
+void sort_array(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf,
+                sort_algorithm_t sa) {
+    if (sa == Merge) {
+        sort_array_merge(arr, single_element_length, element_count, cf);
+    } else if (sa == Insertion) {
+        sort_array_insertion(arr, single_element_length, element_count, cf);
     }
 }
-
--- Sort/csort.h
+++ Sort/csort.h
@@ -3,11 +3,9 @@
 #include "cbase.h"
 /**
  * sort consecutive array by different algorithms.
-*/
-enum sort_algorithm_inner {
-    Merge,
-    Insertion
-};
+ */
+enum sort_algorithm_inner { Insertion = 0, Merge };
 typedef enum sort_algorithm_inner sort_algorithm_t;
-void sort_array(generic_data_t arr, int element_length, int element_count, compare_function_t cf, sort_algorithm_t sa);
-#endif//CSORT_H
+void sort_array(generic_data_t arr, int single_element_length, int element_count, compare_function_t cf,
+                sort_algorithm_t sa);
+#endif  // CSORT_H
