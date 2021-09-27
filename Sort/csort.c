baa74349bdf25c207dd419c58ddbfb526890f023
wuzhenhai <2663976081@qq.com>
2021-09-27 03:59:20 -0700

    code

--- Sort/csort.c
+++ Sort/csort.c
@@ -1,5 +1,6 @@
 #include "csort.h"
 
+#include <alloca.h>
 #include <string.h>
 
 #define ARR(idx) (void*)((char*)(arr) + (single_element_size) * (idx))
@@ -7,8 +8,9 @@
 void sort_array_merge(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf) {}
 
 void sort_array_insertion(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf) {
+    void* key = alloca(single_element_size);
     for (int j = 1; j < element_count; j++) {
-        void* key = ARR(j);
+        memcpy(key, ARR(j), single_element_size);
         int i = j - 1;
         while (i >= 0 && cf(ARR(i), key) > 0) {
             memcpy(ARR(i + 1), ARR(i), single_element_size);
