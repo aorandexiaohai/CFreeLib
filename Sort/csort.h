bf5a5f91759cec4b2b1f3e83f724f7fc2fbe5a6b
wuzhenhai <2663976081@qq.com>
2021-09-27 04:02:40 -0700

    code

--- Sort/csort.h
+++ Sort/csort.h
@@ -6,6 +6,10 @@
  */
 enum sort_algorithm_inner { Insertion = 0, Merge };
 typedef enum sort_algorithm_inner sort_algorithm_t;
+/**
+ * @brief if single_element_size is very large, it may cause the over flow of stack frame. Because when copying
+ * intermediate memory, 'alloca' is used, not 'malloc'.
+ */
 void sort_array(generic_data_t arr, int single_element_size, int element_count, compare_function_t cf,
                 sort_algorithm_t sa);
 #endif  // CSORT_H
