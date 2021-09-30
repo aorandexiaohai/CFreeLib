fd36eef21d91395cccd5801cefc8841f5ae5de95
xxx <xx@qqqq.com>
2021-09-30 08:54:49 +0800

    code

--- Array/carray.c
+++ Array/carray.c
@@ -1,8 +1,12 @@
 #include "carray.h"
 
+#include <alloca.h>
+#include <stdlib.h>
+#include <string.h>
+
 void shuffle_array(generic_data_t arr, int single_element_size, int element_count) {
     void* tmp = alloca(single_element_size);
-    for (int i = single_element_size - 1; i >= 1; i--) {
+    for (int i = element_count - 1; i >= 1; i--) {
         void* ai = fetch_element_address(arr, single_element_size, i);
         int j = rand() % (i + 1);
         void* aj = fetch_element_address(arr, single_element_size, j);
