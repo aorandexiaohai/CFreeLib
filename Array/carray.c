83bb69b5073d3df028d69306664afbbefb5a51b8
xxx <xx@qqqq.com>
2021-09-30 11:53:45 +0800

    code;

--- Array/carray.c
+++ Array/carray.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 void swap_content(generic_data_t data_loc1, generic_data_t data_loc2, generic_data_t tmp, int element_size) {
-    if(data_loc1 == data_loc2) return;
+    if (data_loc1 == data_loc2) return;
     memcpy(tmp, data_loc1, element_size);
     memcpy(data_loc1, data_loc2, element_size);
     memcpy(data_loc2, tmp, element_size);
@@ -16,7 +16,6 @@ void shuffle_array(generic_data_t arr, int single_element_size, int element_coun
     int i = 0;
     for (i = element_count - 1; i >= 1; i--) {
         int j = rand() % (i + 1);
-        if (i == j) continue;
         swap_content(fetch_element_address(arr, single_element_size, i),
                      fetch_element_address(arr, single_element_size, j), tmp, single_element_size);
     }
