b71d44975580876e4724650ddf560a9064278144
wuzhenhai <1839580742@qq.com>
2021-10-04 18:18:56 +0800

    code

--- Sort/test/test.c
+++ Sort/test/test.c
@@ -12,7 +12,8 @@ int com_int(generic_data_t data1_loc, generic_data_t data2_loc) {
     int v2 = *((int*)data2_loc);
     if (v1 < v2) {
         return -1;
-    } else if (v1 == v2) {
+    }
+    else if (v1 == v2) {
         return 0;
     }
     return 1;
@@ -21,7 +22,7 @@ int com_int(generic_data_t data1_loc, generic_data_t data2_loc) {
 typedef int (*func_t)(const void* data1_loc, const void* data2_loc);
 
 #define LENGTH1 100
-#define TEST_COUNT 1000
+#define TEST_COUNT 10000
 #define PERFORMACE_TEST_COUNT 10
 int main() {
     srand(0);
@@ -29,53 +30,91 @@ int main() {
     int i = 0;
     int k = 0;
     int j = 0;
-    for (alg = 0; alg < SortAlgorithmCount; alg++) {
-        for (i = 0; i < TEST_COUNT; i++) {
-            int arr[LENGTH1] = {};
-            int len = (rand() % LENGTH1);
-            for (k = 0; k < len; k++) {
-                arr[k] = rand() % len;
+    {
+        for (alg = 0; alg < SortAlgorithmCount; alg++) {
+            for (i = 0; i < TEST_COUNT; i++) {
+                int arr[LENGTH1] = {};
+                int len = (rand() % LENGTH1);
+                for (k = 0; k < len; k++) {
+                    arr[k] = rand() % len;
+                }
+                shuffle_array(arr, sizeof(int), len);
+                int arr_copy[LENGTH1] = {};
+                memcpy(arr_copy, arr, sizeof(int) * len);
+                sort_array(arr, sizeof(int), len, com_int, alg);
+                qsort(arr_copy, len, sizeof(int), (func_t)com_int);
+                for (k = 0; k < len; k++) {
+                    assert(arr[k] == arr_copy[k]);
+                }
             }
-            shuffle_array(arr, sizeof(int), len);
-            int arr_copy[LENGTH1] = {};
-            memcpy(arr_copy, arr, sizeof(int) * len);
-            sort_array(arr, sizeof(int), len, com_int, alg);
-            qsort(arr_copy, len, sizeof(int), (func_t)com_int);
-            for (k = 0; k < len; k++) {
-                assert(arr[k] == arr_copy[k]);
+        }
+        printf("unit test success\n\n-------------------------------------------------------------\n");
+    }
+    {
+        int len = 10000;
+        int* arr = malloc(len * sizeof(int));
+        int left = len;
+        int base = 0;
+        while (left > 0) {
+            int duplicates = rand() % 10;
+            int v = rand() % len;
+            if (duplicates > left) {
+                duplicates = left;
             }
+            for (i = 0; i < duplicates; i++) {
+                arr[base++] = v;
+            }
+            left -= duplicates;
         }
-    }
-    printf("unit test success\n\n-------------------------------------------------------------\n");
-    int len = 10000;
-    int* arr = malloc(len * sizeof(int));
-    int left = len;
-    int base = 0;
-    while (left > 0) {
-        int duplicates = rand() % 10;
-        int v = rand() % len;
-        if (duplicates > left) {
-            duplicates = left;
-        }
-        for (i = 0; i < duplicates; i++) {
-            arr[base++] = v;
-        }
-        left -= duplicates;
-    }
-    for (alg = 0; alg < SortAlgorithmCount; alg++) {
-        struct timeval begin;
-        gettimeofday(&begin, NULL);
-        for (j = 0; j < PERFORMACE_TEST_COUNT; j++) {
-            shuffle_array(arr, sizeof(int), len);
-            sort_array(arr, sizeof(int), len, com_int, alg);
-        }
-        struct timeval end;
-        gettimeofday(&end, NULL);
+        for (alg = 0; alg < SortAlgorithmCount; alg++) {
+            struct timeval begin;
+            gettimeofday(&begin, NULL);
+            for (j = 0; j < PERFORMACE_TEST_COUNT; j++) {
+                shuffle_array(arr, sizeof(int), len);
+                sort_array(arr, sizeof(int), len, com_int, alg);
+            }
+            struct timeval end;
+            gettimeofday(&end, NULL);
 
-        size_t total_us = (end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec);
-        printf("%20s : %10.2lf ms when sorting %d elements\n", sort_algorithm_strings[alg], total_us / 1.0 / 1000, len);
+            size_t total_us = (end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec);
+            printf("%20s : %10.2lf ms when sorting %d elements\n", sort_algorithm_strings[alg], total_us / 1.0 / 1000, len);
+        }
+        free(arr);
+        printf("performance test success\n\n");
     }
-    free(arr);
-    printf("performance test success\n\n");
+
+    {
+        int len = 1000000;
+        int* arr = malloc(len * sizeof(int));
+        int left = len;
+        int base = 0;
+        while (left > 0) {
+            int duplicates = rand() % 10;
+            int v = rand() % len;
+            if (duplicates > left) {
+                duplicates = left;
+            }
+            for (i = 0; i < duplicates; i++) {
+                arr[base++] = v;
+            }
+            left -= duplicates;
+        }
+        for (alg = 0; alg < SortAlgorithmCount; alg++) {
+            if (alg == Bubble || alg == Insertion || alg == Selection) continue;
+            struct timeval begin;
+            gettimeofday(&begin, NULL);
+            for (j = 0; j < PERFORMACE_TEST_COUNT; j++) {
+                shuffle_array(arr, sizeof(int), len);
+                sort_array(arr, sizeof(int), len, com_int, alg);
+            }
+            struct timeval end;
+            gettimeofday(&end, NULL);
+
+            size_t total_us = (end.tv_sec - begin.tv_sec) * 1000000 + (end.tv_usec - begin.tv_usec);
+            printf("%20s : %10.2lf ms when sorting %d elements\n", sort_algorithm_strings[alg], total_us / 1.0 / 1000, len);
+        }
+        free(arr);
+        printf("performance test of very large arraies success\n\n");
+    }
     return 0;
 }
