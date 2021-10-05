969e43affac6d6dbef92e8d60ad4a5b3fa69c8ba
wuzhenhai <1839580742@qq.com>
2021-10-05 09:27:46 +0800

    code

--- heap/cheap.c
+++ heap/cheap.c
@@ -1,7 +1,48 @@
 #include "cheap.h"
+#include "carray.h"
+
+#include <stdlib.h>
+#include <alloca.h>
 
 void max_heapify(generic_data_t arr, int single_element_size, int element_count,
     data_location_compare_function_t cf, int idx)
 {
-    
+    for (;;)
+    {
+        int largest = idx;
+        void* a_idx = fetch_element_address(arr, single_element_size, idx);
+        void* a_opt = a_idx;
+
+        int left = 2 * idx + 1;
+        int right = 2 * idx + 2;
+        if (left < element_count)
+        {
+            void* a_left = fetch_element_address(arr, single_element_size, left);
+            int cmp = cf(a_left, a_idx);
+            if (cmp > 0) {
+                largest = left;
+                a_opt = a_left;
+            }
+        }
+        if (right < element_count)
+        {
+            void* a_right = fetch_element_address(arr, single_element_size, right);
+            int cmp = cf(a_right, a_idx);
+            if (cmp > 0) {
+                largest = right;
+                a_opt = a_right;
+            }
+        }
+        if (largest != idx)
+        {
+            void* tmp = alloca(single_element_size);
+            swap_content(a_idx, a_opt, tmp, single_element_size);
+            idx = largest;
+        }
+        else
+        {
+            break;
+        }
+    }
+
 }
