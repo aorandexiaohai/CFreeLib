f05100743d74367de7bea6c9d59c96f38145f775
wuzhenhai <2663976081@qq.com>
2021-10-05 20:08:45 -0700

    code

--- Base/cbase.h
+++ Base/cbase.h
@@ -11,6 +11,11 @@ typedef generic_data_t (*generic_function_t)(generic_data_t);
  * @brief swap the content in data_loc1 and data_loc2.
  *
  * @param data_swap_loc it is a temporary swap memory
-*/
+ */
 void swap_content(generic_data_t data_loc1, generic_data_t data_loc2, generic_data_t data_swap_loc, int element_size);
+
+static bool is_small_endien() {
+    int i = 1;
+    return *((char *)(&i));
+}
 #endif
